175d23ceca8dac8dbcc1ce7c5594e4dd4bad59da
MT <matt.git@protonmail.com>
2022-05-07 20:00:34 +1000

    Implemented concurrent call to GraphQL API
    Co-authored-by: reteppeter <reteppeter@users.noreply.github.com>

--- src/graphQLAPI.ts
+++ src/graphQLAPI.ts
@@ -1,3 +1,4 @@
+import { getAccessToken } from "./utils";
 import {
 	GraphQlQueryResponseData,
 	GraphqlResponseError,
@@ -60,13 +61,13 @@ export type RepoEdge = {
 }
 
 export type GraphResponse = {
-    errors: {
-        message: string }[] | null
+    errors?: {
+        message: string }[]
 	rateLimit: {
 		cost: number,
 		remaining: number,
 		resetAt: string
-		
+
 	},
 	organization: {
 		id: string
@@ -80,20 +81,91 @@ export type GraphResponse = {
 	}
 }
 
-export async function queryGraphQL(organisation: string, accessToken: string, repoCursor: string | null) : Promise<GraphQlQueryResponseData> {
-  
-  const graphqlWithAuth = graphql.defaults({
-    headers: {
-      authorization: `token ${accessToken}`,
-      accept: `application/vnd.github.hawkgirl-preview+json`,
-    },
-  });
-  let response: GraphQlQueryResponseData;
+export type OrgRepos = {
+    errors?: {
+        message: string }[]
+	rateLimit: {
+		cost: number,
+		remaining: number,
+		resetAt: string
+
+	},
+	organization: {
+		id: string
+		repositories: {
+			edges: {
+				node :{
+					pushedAt: string,
+					url: string,
+					updatedAt: string,
+					name: string
+				}
+				cursor: string
+			}[]
+			pageInfo: {
+				endCursor: string,
+				hasNextPage: boolean
+			}
+		}
+	}
+}
+
+export async function queryGraphQL(query: string, param: any) : Promise<GraphQlQueryResponseData> {
+	const graphqlWithAuth = graphql.defaults({
+	  headers: {
+		authorization: `token ${getAccessToken()}`,
+		accept: `application/vnd.github.hawkgirl-preview+json`,
+	  },
+	});
+	let response: GraphQlQueryResponseData = graphqlWithAuth(query, param)
+	return response
+}
+
+
+	export async function queryRepositories(organisation: string, repoCursor: string | null) : Promise<GraphQlQueryResponseData> {
+		let query: string =
+			`
+			query OrgRepos($queryString: String!) {
+				rateLimit {
+				  cost
+				  remaining
+				  resetAt
+				}
+				organization(login: $queryString) {
+				  id
+				  repositories(first: 100, orderBy: {field: PUSHED_AT, direction: DESC}) {
+					totalCount
+					edges {
+					  node {
+						pushedAt
+						url
+						updatedAt
+						name
+					  }
+					  cursor
+					}
+					pageInfo {
+					  endCursor
+					  hasNextPage
+					}
+				  }
+				}
+			  }
+			`
+			let param = {
+				queryString: organisation,
+				numOfPages: 100,
+				repoCursor: repoCursor,
+			}
+  		return queryGraphQL(query, param)
+
+	}
+export async function queryDependencies(organisation: string, numOfPages, repoCursor: string | null) : Promise<GraphQlQueryResponseData> {
 
   // refer to this on how to query different branches/ref,
   // https://stackoverflow.com/questions/51504760/how-to-get-all-repos-that-contain-a-certain-branch-on-githubs-graphql-api
-  // TODO: limit to default branch, current setup no working 
-  response = graphqlWithAuth(
+  // TODO: limit to default branch, current setup no working
+  let query: string =
     `
 	query orgRepos($queryString: String!, $numOfPages: Int!, $repoCursor: String, $dependencyLimit: Int!) {
 		rateLimit{
@@ -103,7 +175,7 @@ export async function queryGraphQL(organisation: string, accessToken: string, re
 		   }
 		 organization(login: $queryString) {
 		 id
-		 repositories(first: $numOfPages, after: $repoCursor , orderBy: {field: CREATED_AT, direction: ASC
+		 repositories(first: $numOfPages, after: $repoCursor , orderBy: {field: PUSHED_AT, direction: DESC
 		   }) {
 		   edges {
 		   node {
@@ -159,14 +231,13 @@ export async function queryGraphQL(organisation: string, accessToken: string, re
 		   }
 	   }
    }
-        `,
-    {
+        `
+	let param = {
       queryString: organisation,
-      numOfPages: 5,
+      numOfPages: numOfPages,
       repoCursor: repoCursor,
 	  dependencyLimit: 10
     }
-  )
 
-  return response
+  return queryGraphQL(query, param)
 }
